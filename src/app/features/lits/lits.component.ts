eaf7ee829960ce832566b8687af625a3d3b82f25
joao.calza <jpcalza29@gmail.com>
2024-08-07 20:29:02 -0300

    update

--- src/app/features/lits/lits.component.ts
+++ src/app/features/lits/lits.component.ts
@@ -1,10 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { ProductsService } from '../../shared/services/products.service';
+import { MatCardModule } from "@angular/material/card"
+import { MatButtonModule } from "@angular/material/button"
 
 @Component({
   selector: 'app-lits',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './lits.component.html',
   styleUrl: './lits.component.css'
 })
@@ -12,10 +15,10 @@ export class LitsComponent {
 
   products: any[]= [];
 
-  httpClient = inject(HttpClient);
+  productsService = inject(ProductsService);
 
   ngOnInit(){
-    this.httpClient.get<any>('/api/products').subscribe((products) =>{
+    this.productsService.getAll().subscribe((products) =>{
       this.products = products
     });
   }

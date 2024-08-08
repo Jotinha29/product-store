5b23b77f259bee767e0a20bc1aaa49be87602954
joao.calza <jpcalza29@gmail.com>
2024-08-08 12:54:25 -0300

    add: rota para create component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { LitsComponent } from './features/list/list.component';
+import { CreateComponent } from './features/create/create.component';
 
-export const routes: Routes = [{
+export const routes: Routes = [
+    {
     path: '',
     component: LitsComponent
-}];
+    },
+    {
+    path: 'create-product',
+    component: CreateComponent,
+    },
+];

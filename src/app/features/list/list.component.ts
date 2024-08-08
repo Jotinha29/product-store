497a59ec307c3eac547704463b85ae8c8893e3a9
joao.calza <jpcalza29@gmail.com>
2024-08-07 23:26:06 -0300

    feat: add "criar novo produto"

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -2,12 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
   selector: 'app-lits',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })

a1ede3707b7c5426dde72086d5238a386e2ad164
joao.calza <jpcalza29@gmail.com>
2024-08-13 16:31:14 -0300

    feat: add função delete ao produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,11 +1,38 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { CardComponent } from './components/card/card.component';
 import { Router, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { Product } from '../../shared/interfaces/product.interface';
+import { filter } from 'rxjs';
 
+@Component({
+  selector: 'app-confirmation-dialog',
+  template: `
+    <h2 mat-dialog-title>Deletar produto</h2>
+    <mat-dialog-content>
+      Tem certeza que deseja deletar esse produto?
+    </mat-dialog-content>
+    <mat-dialog-actions align="end">
+      <button mat-button (click)="onNo()">Não</button>
+      <button mat-raised-button color="accent" (click)="onYes()" cdkFocusInitial>Confirmar</button>
+    </mat-dialog-actions>
+  `,
+  standalone: true,
+  imports: [MatButtonModule, MatDialogModule],
+})
+export class confirmationDialogComponent {
+  matDialogRef = inject(MatDialogRef);
+
+  onNo() {
+    this.matDialogRef.close(false);
+  }
+
+  onYes() {
+    this.matDialogRef.close(true);
+  }
+}
 
 @Component({
   selector: 'app-lits',
@@ -16,18 +43,34 @@ import { Product } from '../../shared/interfaces/product.interface';
 })
 export class LitsComponent {
 
-  products: any[]= [];
+  products: any[] = [];
 
   productsService = inject(ProductsService);
   router = inject(Router);
+  matDialog = inject(MatDialog);
 
-  ngOnInit(){
-    this.productsService.getAll().subscribe((products) =>{
+  ngOnInit() {
+    this.productsService.getAll().subscribe((products) => {
       this.products = products
     });
   }
 
-  onEdit(product: Product){
-      this.router.navigate(['/edit-product', product.id]);
+  onEdit(product: Product) {
+    this.router.navigate(['/edit-product', product.id]);
+  }
+
+  onDelete(product: Product) {
+    this.matDialog.open(confirmationDialogComponent)
+      .afterClosed()
+      .pipe(filter(answer => answer === true))
+      .subscribe(() => {
+        this.productsService.delete(product.id)
+          .subscribe(() => {
+            this.productsService.getAll().subscribe((products) => {
+              this.products = products
+            });
+          });
+
+      });
   }
 }
